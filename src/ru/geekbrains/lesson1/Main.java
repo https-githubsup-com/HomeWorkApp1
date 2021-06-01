ef285a2848568774cd78ac998ed6b0753baa2325
Irina <70594426+irinazheltisheva@users.noreply.github.com>
2021-06-01 15:46:26 +0300

    Initial commit

---
+++ src/ru/geekbrains/lesson1/Main.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Создайте метод printThreeWords(), который при вызове
+        // должен отпечатать в столбец три слова:
+        //_Orange
+        //_ Banana
+        //_ Apple
+
+     printThreeWords();
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
